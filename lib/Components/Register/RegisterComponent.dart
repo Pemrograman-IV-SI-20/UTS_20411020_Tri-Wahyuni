52819e7f8c428edc932adda7b39b492120275fca
Triwahyuni99 <triwahyuni20411020@gmail.com>
2022-10-26 23:57:46 +0700

    Toko Gitar Tri

---
+++ lib/Components/Register/RegisterComponent.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:simple_shadow/simple_shadow.dart';
+import 'package:toko_gitar_flutter/components/register/RegisterForm.dart';
+import 'package:toko_gitar_flutter/size_config.dart';
+import 'package:toko_gitar_flutter/utils/constants.dart';
+
+class RegisterComponent extends StatefulWidget {
+  @override
+  _RegisterComponent createState() => _RegisterComponent();
+}
+
+class _RegisterComponent extends State<RegisterComponent> {
+
+  @override
+  Widget build(BuildContext context){
+    return SafeArea(
+
+      child: SizedBox (
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenHeight(20)),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    "REGISTRASI",
+                    style: TextStyle(fontSize: 30, color: kColorRedSlow),
+                  ),
+                  SizedBox(
+                    height: SizeConfig.screenHeight *0.04,
+                  ),
+                   SizedBox(
+                    height: SizeConfig.screenHeight *0.04,
+                  ),
+                  // SimpleShadow(
+                  //   child: Image.asset("assets/images/ukelele.png", 
+                  //   height: 150, 
+                  //   width: 202,
+                  //   ),
+                  //   opacity: 0.5,
+                  //   color: kColorGreen,
+                  //   offset: Offset(5, 5),
+                  //   sigma: 2,
+                  //   ),
+                    Padding(padding: EdgeInsets.only(left: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Registrasi !", style: mTitleStyle,
+                        )
+                      ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SignUpform()
+                ],
+                ),
+            ),
+        ),
+      ),
+    );
+  }
+}
